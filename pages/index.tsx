8db128a49ea56904f623b9402496a5834adbea96
Doloman040206 <166598565+Doloman040206@users.noreply.github.com>
2025-09-05 10:03:50 +0200

    little fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,9 @@ const App = () => {
 		<Head>
 			<title>My page title</title>
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous"></link>
+			<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossOrigin="anonymous"></script>
+    		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" crossOrigin="anonymous"></script>
+    		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" crossOrigin="anonymous"></script>
 		</Head>
 		<div className="container">
 			<nav className="navbar navbar-expand-lg navbar-light p-0">
@@ -25,7 +28,7 @@ const App = () => {
 							<a className="nav-link" href="#Про нас">Про нас</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#pizzas">Наші піци </a>
+							<a className="nav-link" href="#choise">Наше меню </a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="#portfolio">Наші найбільші філіали</a>

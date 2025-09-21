fd7f62cab26a31bf996af128dc00a8255991b81c
Doloman040206 <166598565+Doloman040206@users.noreply.github.com>
2025-09-21 11:06:22 +0200

    Changing API: work with photos and their conections to pizzas and weather

--- pages/components/PublicPizaList.tsx
+++ pages/components/PublicPizaList.tsx
@@ -1,55 +1,41 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PizaObject } from '../admin/types';
 
-
-
-
-
-
-export function PublicPizaList() {
-    const fetchPizaList = async () => {
-		const responce = await fetch('/api/piza');
-		const data = (await responce.json())
-		// debugger
-		console.log('fetchPiza response', data)
-		setPizaList(data)
-	}
-
+    export function PublicPizaList() {
     const [pizaList, setPizaList] = useState<PizaObject[]>([]);
-	useEffect(() => {
-		fetchPizaList()
-	}, [])
+    useEffect(() => {
+    fetch('/api/piza')
+      .then(res => res.json())
+      .then(setPizaList);
+    }, []);
 
-    const pizzaImages =['./img/99.jpg', './img/100.jpg', './img/101.jpg']
+  
+    const imagesByName: Record<string, string> = {
+    Margherita: './img/99.jpg',
+    Hawai: './img/100.jpg',
+    Pepperoni: './img/101.jpg',
+    };
 
-    
-    return <>
-        {pizaList.length > 0 ? (
-            pizaList.map((item, index) => {
-                const imageIndex = index % pizzaImages.length;
-                return ( 
-                <div key={index} style={{ marginBottom:'10px', marginLeft:'30px', fontSize:'1.2rem'}}>
-                    
-                <div style={{fontSize:'1.2rem',color:'#333'}}>   
-                    Piza <b>"{item.name}"</b> with {item.ingridients} at <b>{item.price}</b>$
-                    </div>
-                    <div>
-                   <img src={pizzaImages[imageIndex]} alt="piza" style={{width:'300px', marginLeft:'500px', marginTop:'20px'}} />
-                </div>
-                </div>
-            )
-            })
-        ) : (
-            <div
-                style={{
-                    textAlign: 'center',
-                    fontSize: '1.2rem',
-                    color: '#777'
-                }}
-            >
-                No items in the list
+    return (
+    <>
+      {pizaList.length > 0 ? (
+        pizaList.map((item, index) => (
+          <div key={index} style={{ marginBottom: '10px', marginLeft: '30px', fontSize: '1.2rem' }}>
+            <div style={{ fontSize: '1.2rem', color: '#333' }}>
+              Piza <b>"{item.name}"</b> with {item.ingridients} at <b>{item.price}</b>$
             </div>
-        )}
+            <div>
+              <img
+                src={(imagesByName as any)[item.name]} 
+                alt={item.name}
+                style={{ width: '300px', marginLeft: '500px', marginTop: '20px' }}
+              />
+            </div>
+          </div>
+        ))
+      ) : (
+        <div style={{ textAlign: 'center', fontSize: '1.2rem', color: '#777' }}>No items in the list</div>
+      )}
     </>
-   
+  );
 }
--- pages/components/WeatherWidget.tsx
+++ pages/components/WeatherWidget.tsx
@@ -1,28 +1,59 @@
-import React, { use, useEffect, useState } from 'react';
-import { PizaObject } from '../admin/types';
-
-
-
-
-
+import React, { useEffect, useState } from 'react';
 
 export function PublicWeatherWidget() {
-    const fetchWeather = async () => {
-		const responce = await fetch('/api/weather');
-		const data = (await responce.json())
-		console.log('fetchWeather response', data)
-		setWeather(data.weather)
-	}
+  const [w1, setW1] = useState('');
+  const [w2, setW2] = useState('');
+  const [w3, setW3] = useState('');
 
-    const [weather, setWeather] = useState('');
-	useEffect(() => {
-		fetchWeather()
-	}, [])
+  useEffect(() => {
+    fetch('/api/weather')
+      .then(res => res.json())
+      .then(data => setW1(data.weather));
 
-    
-    return <>
-        <div style={{fontSize:'1.2rem',marginLeft:'30px', marginTop:'20px', fontWeight: 'bold'}}>Weather is {weather}</div> 
-		<img src="./img/103.jpg" alt="weather" style={{width:'300px', marginLeft:'530px', marginTop:'40px', marginBottom:'20px'}} />
+    fetch('/api/weather/2')
+      .then(res => res.json())
+      .then(data => setW2(data.weather));
+
+    fetch('/api/weather/3')
+      .then(res => res.json())
+      .then(data => setW3(data.weather));
+  }, []);
+
+  const imagesByWeather: Record<string, string> = {
+    'Clear sky': './img/clear.jpg',
+    'Rain showers: Slight, moderate, and violent': './img/103.jpg',
+	'Rain: Slight, moderate and heavy intensity': './img/103.jpg',
+    'Fog and depositing rime fog': './img/fog.jpg',
+  };
+
+  return (
+    <>
+      <div style={{ fontSize: '1.2rem', marginLeft: '30px', marginTop: '20px', fontWeight: 'bold' }}>
+        Weather in City 1 is {w1}
+      </div>
+      <img
+        src={imagesByWeather[w1]}
+        alt={w1}
+        style={{ width: '300px', marginLeft: '530px', marginTop: '40px', marginBottom: '20px' }}
+      />
+
+      <div style={{ fontSize: '1.2rem', marginLeft: '30px', marginTop: '20px', fontWeight: 'bold' }}>
+        Weather in City 2 is {w2}
+      </div>
+      <img
+        src={imagesByWeather[w2]}
+        alt={w2}
+        style={{ width: '300px', marginLeft: '530px', marginTop: '40px', marginBottom: '20px' }}
+      />
+
+      <div style={{ fontSize: '1.2rem', marginLeft: '30px', marginTop: '20px', fontWeight: 'bold' }}>
+        Weather in City 3 is {w3}
+      </div>
+      <img
+        src={imagesByWeather[w3]}
+        alt={w3}
+        style={{ width: '300px', marginLeft: '530px', marginTop: '40px', marginBottom: '20px' }}
+      />
     </>
-   
+  );
 }
